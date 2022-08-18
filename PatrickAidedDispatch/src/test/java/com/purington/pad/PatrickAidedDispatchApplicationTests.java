036e45d73553cd8b51f499963687ca4d6c3a4e6c
PPurington77 <papurington@gmail.com>
2022-08-18 12:37:56 -0600

    project
    project

---
+++ PatrickAidedDispatch/src/test/java/com/purington/pad/PatrickAidedDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purington.pad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrickAidedDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
